e2533c676e73339466743aa04a3a4d44c65c2eed
joaognx <joaotutors291@gmail.com>
2026-01-14 18:02:50 -0300

    top bar mobile

--- src/components/Topo.jsx
+++ src/components/Topo.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
 import { useState } from "react";
+import { ShoppingBag } from 'lucide-react';
+import { CircleUser } from 'lucide-react';
+import { Link } from "react-router-dom";
 import './Topo.css'
 
+
 function Topo() {
-
   const [menuAberto, setMenuAberto] = useState(false);
-
   const menu = [
     { nome: "Início", link: "/" },
     { nome: "Biquínis", link: "/biquinis" },
@@ -14,40 +15,48 @@ function Topo() {
     { nome: "Masculino", link: "/masculino" },
     { nome: "Acessórios", link: "/acessorios" },
     { nome: "Infantil", link: "/infantil" }
-  ];
+  ]
+
 
   return (
     <header className='header'>
 
       <div className='topbar'>
-        
-         <div
-            className="hamburger"
+
+        <div className='left'>
+          <div
+            className={`hamburger ${menuAberto ? 'aberto' : ''}`}
             onClick={() => setMenuAberto(!menuAberto)}
           >
             ☰
           </div>
-          
-        <div className='left'>
+
           <input
             className="search"
             type="text"
             placeholder="O que você está buscando?"
           />
+
+
         </div>
 
         <Link to="/" className='center'>
           <img src="logospng.png" alt="Vitrine do Mar" className='logo' />
         </Link>
 
+
         <div className="right">
-          <a href='#'>Meu Carrinho</a>
-          <a href='#'>Minha Conta</a>
+          <a href='#'> <ShoppingBag size={40} color="#1a365d" strokeWidth={1.5} /> </a>
+          <a href='#'><CircleUser size={40} color="#1a365d" strokeWidth={1.5} /></a>
         </div>
       </div>
 
       {/* MENU */}
+
+
       <nav className={`menu ${menuAberto ? 'ativo' : ''}`}>
+
+        
         {menu.map((tipo, i) => (
           <Link
             to={tipo.link}
@@ -57,10 +66,17 @@ function Topo() {
             {tipo.nome}
           </Link>
         ))}
+
+        <div className="search-container-mobile">
+      <input className="search-mobile" type="text" placeholder="Buscar produtos..." />
+    </div> 
       </nav>
 
+
     </header>
-  );
+
+
+  )
 }
 
-export default Topo;
+export default Topo
